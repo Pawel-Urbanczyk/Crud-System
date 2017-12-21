1d6645c595a03a714f33b46bffae45914fb732de
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2017-12-21 17:19:50 +0100

    Ostylowanie formularza, do poprawy

--- user/signup.php
+++ user/signup.php
@@ -3,51 +3,51 @@
         <h3>Crud System User Sign Up</h3>
     </div>
 
-    <div class="row">
-        <p>This is a system that aloows users to create, read, updtae and delete information.</p>
+    <div class="row sub-msg">
+        <p>This is a system that aloows users to create, read, update and delete information.</p>
     </div>
 
-    <div class="row">
+    <div class="row signup">
         <div class="row">
             <h3>Welcome, please sign up!</h3>
         </div>
 
         <form action="" method="post" class="form-horizontal">
             <!--Email-->
-            <div class="row">
-                <label for="email" class="col-sm-2">Email: </label>
-                <div class="col-sm-10 form-group">
+            <div class="row form-group input-group">
+                <label for="email" class="col-sm-4">Email: </label>
+                <div class="col-sm-8">
                     <input type="email" name="email" id="email" class="form-control">
                 </div>
             </div>
 
             <!--First name-->
-            <div class="row">
-                <label for="firstname" class="col-sm-2">First name: </label>
-                <div class="col-sm-10 form-group">
+            <div class="row form-group input-group">
+                <label for="firstname" class="col-sm-4">First name: </label>
+                <div class="col-sm-8">
                     <input type="text" name="firstname" id="firstname" class="form-control">
                 </div>
             </div>
 
             <!--Last name-->
-            <div class="row">
-                <label for="lastname" class="col-sm-2">Last name: </label>
-                <div class="col-sm-10 form-group">
+            <div class="row form-group input-group">
+                <label for="lastname" class="col-sm-4">Last name: </label>
+                <div class="col-sm-8">
                     <input type="text" name="lastname" id="lastname" class="form-control">
                 </div>
             </div>
 
             <!--Password-->
-            <div class="row">
-                <label for="password" class="col-sm-2">Password: </label>
-                <div class="col-sm-10 form-group">
+            <div class="row form-group input-group">
+                <label for="password" class="col-sm-4">Password: </label>
+                <div class="col-sm-8">
                     <input type="password" name="password" id="password" class="form-control">
                 </div>
             </div>
 
             <!--Submit-->
             <div class="col-xs-12 form-group">
-                <input type="submit" name="submit" id="submit" class="form-control" value="Send">
+                <input type="submit" name="submit" id="submit" class="form-control btn-primary" value="Send">
             </div>
         </form>
     </div>
