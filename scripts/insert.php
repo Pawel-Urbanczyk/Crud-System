fbad2f11d153ef96ea2914f1ef6355e2de6750fd
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2017-12-26 23:44:06 +0100

    Oprogramowanie insert.php - do skonczenia

--- scripts/insert.php
+++ scripts/insert.php
@@ -8,5 +8,23 @@
         $lastname = $_POST['lastname'];
         $email = $_POST['email'];
         $password = $_POST['password'];
+
+        $sql = "SELECT * FROM signup WHGERE email = '{$email}' ";
+        $sql_query = mysqli_query($conn, $sql);
+        $count = mysqli_num_rows($sql_query);
+
+        if(!empty($firstname) && !empty($lastname) && !empty($email) && !empty($password)){
+
+            if($count > 0){
+                $error1 = "<div class='alert alert-danger alert-dismissable'>
+                              <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                              User with that email already exist.
+                           </div>";
+            }else{
+
+            }
+        }else{
+            ///
+        }
     }
 ?>
